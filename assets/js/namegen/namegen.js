4c912e05ad6dc5f32a08c643a9238e3f2ee165fe
Muhammad Asyraf bin Ibrahim <asyzruffz@gmail.com>
2017-08-16 17:16:42 +0800

    Test another json getter

--- assets/js/namegen/namegen.js
+++ assets/js/namegen/namegen.js
@@ -1,17 +1,42 @@
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+		var status = xhr.status;
+		if (status == 200) {
+			callback(null, xhr.response);
+		} else {
+			callback(status);
+		}
+    };
+    xhr.send();
+};
+
 function generateName() {
 	//var json = $.getJSON({'url': "./data/namedatabase.json", 'async': false});
 	//var database = JSON.parse(json.responseText);
 	
 	var database;
 	
-	$.ajax({
+	getJSON("https://asyzruffz.github.io/Portfolio/data/namedatabase.json", function(err, data) {
+		if (err != null) {
+			alert("Something went wrong: " + err);
+		} else {
+			database = data;
+			console.log(data);
+			alert("Your query count: " + data.query.count);
+		}
+	});
+	
+	/*$.ajax({
          url: "data/namedatabase.json",
          async: false,
          dataType: 'json',
          success: function(response) {
 			database = response.items;
 		}
-	});
+	});*/
 	
 	console.log(database);
 	
