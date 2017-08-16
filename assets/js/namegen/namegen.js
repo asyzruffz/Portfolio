7b85f7e622721038104312509b2f7ccd17bee365
Muhammad Asyraf bin Ibrahim <asyzruffz@gmail.com>
2017-08-16 16:01:29 +0800

    test ajax

--- assets/js/namegen/namegen.js
+++ assets/js/namegen/namegen.js
@@ -1,6 +1,15 @@
 function generateName() {
-	var json = $.getJSON({'url': "./data/namedatabase.json", 'async': false});
-	var database = JSON.parse(json.responseText);
+	//var json = $.getJSON({'url': "./data/namedatabase.json", 'async': false});
+	//var database = JSON.parse(json.responseText);
+	
+	$.ajax({
+         url: "data/namedatabase.json",
+         async: false,
+         dataType: 'json',
+         success: function(response) {
+			database = response.items;
+		}
+	});
 	
 	console.log(database);
 	
