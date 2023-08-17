004c66eb44f2eb3dbf3da299c671fafede21f955
Asyraf <asyzruffz@gmail.com>
2023-08-17 17:48:10 +0800

    Remove flutter test code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,23 +8,11 @@
 //import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:portfolio/portfolio_app.dart';
+//import 'package:portfolio/portfolio_app.dart';
 
 void main() {
   testWidgets('Portfolio smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const PortfolioApp());
-
-    // Verify that our counter starts at 0.
-    //expect(find.text('0'), findsOneWidget);
-    //expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    //await tester.tap(find.byIcon(Icons.add));
-    //await tester.pump();
-
-    // Verify that our counter has incremented.
-    //expect(find.text('0'), findsNothing);
-    //expect(find.text('1'), findsOneWidget);
+    //await tester.pumpWidget(const PortfolioApp());
   });
 }
