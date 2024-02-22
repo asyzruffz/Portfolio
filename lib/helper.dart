6b859215bb32b0750fefdd7175d1b62e24d8ecc7
Asyraf <asyzruffz@gmail.com>
2024-02-22 21:23:45 +0800

    Removed exception when clicking links

--- lib/helper.dart
+++ lib/helper.dart
@@ -3,8 +3,6 @@ import 'package:url_launcher/url_launcher.dart';
 class Helper {
   static Future<void> goUrl(String url) async {
     Uri uri = Uri.parse(url);
-    if (!await launchUrl(uri)) {
-      throw Exception('Could not launch $uri');
-    }
+    await launchUrl(uri);
   }
 }
